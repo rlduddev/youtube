5e28f61025fd342447f4d7285cbd4d8aa0239477
rlduddev <rldudcjswp@naver.com>
2021-12-26 15:15:55 +0900

    show vieolist title

---
+++ src/app.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import './app.css';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&key=AIzaSyCO4CNTRgz3Lj8MN5nfgtEW1seV9Jf74Dk", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos}></VideoList>;
+}
+
+export default App;
