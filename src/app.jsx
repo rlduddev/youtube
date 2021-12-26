d61b9e2811e6bc9f413b8df14ac7963f30eac7a8
rlduddev <rldudcjswp@naver.com>
2021-12-26 17:33:48 +0900

    video-detail

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,23 @@ import { useEffect, useState } from 'react';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 import styles from './app.module.css'
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
+  const [selectedVideo, setSelectedViedo] = useState(null);
+
+  const selectVideo = video => {
+    setSelectedViedo(video);
+  }
+
   const search = query => {
     youtube.search(query)
-      .then(videos => setVideos(videos))
+      .then(videos => {
+        setVideos(videos)
+        setSelectedViedo(null);
+      })
   }
 
   useEffect(() => {
@@ -19,7 +29,19 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search}/>
-      <VideoList videos={videos}></VideoList>
+      <section className={styles.content}>
+        {
+          selectedVideo && (
+            <div className={styles.detail}>
+              <VideoDetail video={selectedVideo}></VideoDetail>
+            </div>
+          )
+        }
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'list' : 'grid'}></VideoList>
+        </div>
+      </section>
+
     </div>);
 }
 
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import VideoItem from '../video_item/video_item';
 import styles from './video_list.module.css'
 
-const VideoList = (props) => (
+const VideoList = ({videos, onVideoClick, display}) => (
             <ul className={styles.videos}>
-                {props.videos.map(video => <VideoItem video={video} key={video.id}></VideoItem>)}
+                {videos.map(video => <VideoItem video={video} key={video.id} onVideoClick={onVideoClick} display={display}></VideoItem>)}
             </ul>
     );
 
